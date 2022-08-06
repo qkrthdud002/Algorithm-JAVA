c3e76e2f649338923f1459e548f58c8141fffb78
qkrthdud002 <qkrthdud002@gmail.com>
2022-08-06 22:58:56 +0900

    0806

--- src/DataStructureBasic0806/leftDayOfYearQ9.java
+++ src/DataStructureBasic0806/leftDayOfYearQ9.java
@@ -2,31 +2,25 @@ package DataStructureBasic0806;
 
 import java.util.Scanner;
 
-// 그 해 경과 일 수를 구함
-public class DayOfYear {
-    // 각 달의 일수
+public class leftDayOfYearQ9 {
     static int[][] mdays = {
             {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, // 평년
             {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}  // 윤년
     };
 
-    // 서기 year년은 윤년인가 (윤년 : 1/ 편년 : 0)
     static int isLeap(int year) {
         return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? 1 : 0;
     }
 
-    // 서기 y년 m월 d일의 그 해 경과 일 수를 구함
     static int dayOfYear(int y, int m, int d) {
-        int days = d;   // 일 수
-
-        for(int i = 1; i < m; i++)  // 1월~(m-1)월의 일 수를 더함
-            days += mdays[isLeap(y)][i - 1];
-        return days;
+        while(--m != 0)
+            d += mdays[isLeap(y)][m - 1];
+        return 365 + isLeap(y) - d;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int retry;  // 다시 한 번
+        int retry;
 
         System.out.println("그 해 경과 일수를 구합니다.");
 
@@ -38,10 +32,10 @@ public class DayOfYear {
             System.out.print("일 : ");
             int day = sc.nextInt();
 
-            System.out.printf("그 해 %d일째 입니다.\n", dayOfYear(year, month, day));
+            System.out.printf(year + "년이 " + dayOfYear(year, month, day) + "일 남았습니다.\n");
 
-            System.out.print("한 번 더 할까요? (1.예/ 0.아니오) : ");
+            System.out.print("한 번 더 할까요? (1.예/2.아니오) : ");
             retry = sc.nextInt();
-        } while(retry == 1);
+        } while (retry == 1);
     }
 }
