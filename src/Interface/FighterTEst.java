e39a0e49e6b88823cd7c342a0cb0a05d825153f8
qkrthdud002 <qkrthdud002@gmail.com>
2022-09-06 21:26:02 +0900

    0906

---
+++ src/Interface/FighterTEst.java
@@ -0,0 +1,35 @@
+package Interface;
+
+public class FighterTEst {
+    public static void main(String[] args) {
+        Fighter f = new Fighter();
+
+        if(f instanceof Unit)
+            System.out.println("f는 Unit클래스의 자손입니다.");
+        if(f instanceof Fightable)
+            System.out.println("f는 Fightable 인터페이스를 구현했습니다.");
+        if(f instanceof Moveable)
+            System.out.println("f는 Movable 인터페이스를 구현했습니다.");
+        if(f instanceof Attackable)
+            System.out.println("f는 Attackable 인터페이스를 구현했습니다.");
+        if(f instanceof Object)
+            System.out.println("f는 Object 클래스의 자손입니다.");
+    }
+}
+
+class Fighter extends Unit implements Fightable {
+    public void move(int x, int y) { /* 내용생략 */ }
+    public void attack(Unit u) { /* 내용생략 */ }
+}
+
+class Unit {
+    int currentHP; // 유닛의 체력
+    int x;         // 유닛의 위치(x좌표)
+    int y;         // 유닛의 위치(y좌표)
+}
+
+interface Fightable extends Moveable, Attackable { }
+
+interface Moveable {void move(int x, int y);}
+
+interface Attackable {void attack(Unit u);}
