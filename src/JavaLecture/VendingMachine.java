9b3016bdcca3d7c6bb41e6d5ad378db7deeb55fe
qkrthdud002 <qkrthdud002@gmail.com>
2023-02-22 23:45:50 +0900

    230222

---
+++ src/JavaLecture/VendingMachine.java
@@ -0,0 +1,9 @@
+package JavaLecture;
+
+public class VendingMachine {
+    // field(가지는 것들)
+
+    // 생성자
+
+    // method
+}
---
+++ src/JavaLecture/VendingMachineMain.java
@@ -0,0 +1,8 @@
+package JavaLecture;
+
+public class VendingMachineMain {
+    public static void main(String[] args) {
+        VendingMachine vm1 = new VendingMachine();
+        VendingMachine vm2 = new VendingMachine();
+    }
+}
