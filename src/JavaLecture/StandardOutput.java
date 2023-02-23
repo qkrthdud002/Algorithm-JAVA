fe525f44a4feb388b7b83e83d8276c41fddce992
qkrthdud002 <qkrthdud002@gmail.com>
2023-02-23 23:17:21 +0900

    230223

--- src/JavaLecture/StandardOutput.java
+++ src/JavaLecture/StandardOutput.java
@@ -1,5 +1,28 @@
 package JavaLecture;
 
 public class StandardOutput {
+    public void println(boolean b) {
+        System.out.println(b);
+    }
 
+    public void println(int i) {
+        System.out.println(i);
+    }
+
+    public void println(double d) {
+        System.out.println(d);
+    }
+
+    public void println(String s) {
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        StandardOutput output = new StandardOutput();
+
+        output.println(true);
+        output.println(100);
+        output.println(12.3);
+        output.println("hello");
+    }
 }
